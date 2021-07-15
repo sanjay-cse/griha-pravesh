23faf1f4cd9e217ad525927cdb588aed0bed8914
Sanjay Kumar <sanjay.cse@nitjsr.ac.in>
2021-07-15 23:13:32 +0530

    Update background and image

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import image from './image.png';
+import image2 from './mummy papa.png';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <img src={image} className="App-logo" alt="logo" />
+      <div className="App-col-flex-box">
+        <img src={image2} className="App-logo" alt="logo" />
         <p>
           Mrs. and Dr. Sanjay Kumar invite you to their Grih Pravesh.
         </p>
         <p>22nd July, 2021</p>
         <span>Gayatri Kripa(Duplex)</span>
         <span>Hari Om Nagar Road Number 5</span>
-        <span>Adityapur</span>
-      </header>
+        <span>Adityapur, Jamshedpur</span>
+      </div>
     </div>
   );
 }
