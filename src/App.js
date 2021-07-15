86d9f37b6f88d68295e552f973bf7c781b9fadd8
Sanjay Kumar <sanjay.cse@nitjsr.ac.in>
2021-07-15 23:30:48 +0530

    Remove unused images and rename

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-import image from './image.png';
-import image2 from './mummy papa.png';
-import ganeshJi from './ganesh ji vector1.png';
+import mummypapa from './mummypapa.png';
+import ganeshJi from './ganeshji.png';
 
 function App() {
   return (
     <div className="App">
       <div className="App-col-flex-box">
         <img src={ganeshJi} className="ganeshji" alt="shri ganesh"/>
-        <img src={image2} className="mummy-and-papa" alt="logo" />
+        <img src={mummypapa} className="mummy-and-papa" alt="Mrs. and Dr. Sanjay Kumar" />
         <p>
           Mrs. and Dr. Sanjay Kumar invite you to their Grih Pravesh.
         </p>
