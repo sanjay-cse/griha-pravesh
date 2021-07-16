6aadbfe7036e4d8c051b443e207fb16059061f8f
Sanjay Kumar <sanjay.cse@nitjsr.ac.in>
2021-07-16 09:12:46 +0530

    Update message and remove unused files

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ function App() {
       <div className="App-col-flex-box">
         <img src={ganeshJi} className="ganeshji" alt="shri ganesh"/>
         <img src={mummypapa} className="mummy-and-papa" alt="Mrs. and Dr. Sanjay Kumar" />
-        <p>Mrs. and Dr. Sanjay Kumar invite you to their Grih Pravesh.<br/>22nd July, 2021</p>
+        <p>Mrs. and Dr. Sanjay Kumar cordially invite you to the auspicious occasion of Griha Pravesh of our new residence on<br/>22nd July, 2021</p>
 
         <p>
-        Gayatri Kripa (Duplex)<br/>
-        Hari Om Nagar Road Number 5<br/>
+        Gayatri Kripa (Duplex),<br/>
+        Heaven River View,<br/>
+        Road Number 5, Hari Om Nagar,<br/>
         Adityapur, Jamshedpur
         </p>
       </div>
